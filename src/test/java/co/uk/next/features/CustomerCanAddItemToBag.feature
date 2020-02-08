a60df6b6ed3f3b0cb17f622887f0470b105f0eaf
Ola <olawunmi263@yahoo.com>
2020-02-08 04:05:05 +0000

    This test has failed yet I'm excited

--- src/test/java/co/uk/next/features/CustomerCanAddItemToBag.feature
+++ src/test/java/co/uk/next/features/CustomerCanAddItemToBag.feature
@@ -3,14 +3,14 @@ Feature: Add to basket
   I want the ability to add item into basket
   So that I can decide to buy or not
 
-  Background: Given I navigate to Next.co.uk
+  Scenario Outline: User can successfully add item to cart
+
+    Given I navigate to Next.co.uk
     And I enter “<searchItem>” in search bar
     And search result page is displayed
     And I click on “<itemNumber>”
     And product detail page is displayed
-
-  Scenario Outline: User can successfully add item to cart
-    When I select “<productSize>”from size Drop down
+    When I select “<productSize>” from size Drop down
     And I click ‘add to bag’
     Then number of items in bag is displayed
     And items in bag are displayed
---
+++ src/test/java/co/uk/next/stepDefinitions/AddItemsToBagStepDefinitions.java
@@ -0,0 +1,72 @@
+package co.uk.next.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AddItemsToBagStepDefinitions {
+
+    @Given("I navigate to Next.co.uk")
+    public void i_navigate_to_Next_co_uk() {
+
+    }
+
+    @Given("I enter “girls shoes” in search bar")
+    public void i_enter_girls_shoes_in_search_bar() {
+
+    }
+
+    @Given("search result page is displayed")
+    public void search_result_page_is_displayed() {
+
+    }
+
+    @Given("I click on “{int}”")
+    public void i_click_on(Integer int1) {
+
+    }
+
+    @Given("product detail page is displayed")
+    public void product_detail_page_is_displayed() {
+
+    }
+
+    @When("I select “{int} EU {int}”from size Drop down")
+    public void i_select_EU_from_size_Drop_down(Integer int1, Integer int2) {
+
+    }
+
+    @When("I click ‘add to bag’")
+    public void i_click_add_to_bag() {
+
+    }
+
+    @Then("number of items in bag is displayed")
+    public void number_of_items_in_bag_is_displayed() {
+
+    }
+
+    @Then("items in bag are displayed")
+    public void items_in_bag_are_displayed() {
+
+    }
+
+    @Then("total of items in bag is displayed")
+    public void total_of_items_in_bag_is_displayed() {
+
+    }
+
+    @Then("checkout button is highlighted")
+    public void checkout_button_is_highlighted() {
+
+    }
+
+
+
+
+
+
+}
+
+
+
